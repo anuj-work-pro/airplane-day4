1f1f76b4ed54ddab3b79f943612aa3b8574d5460
Anuj-works <vasutomar0001@gmail.com>
2026-04-27 08:54:07 +0530

    fix pre-commit formatting

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,41 +1,48 @@
+import random
+
 import frappe
 from frappe.model.document import Document
-import random
-import string
+
 
 class AirplaneTicket(Document):
-    def validate(self):
-        self.calculate_total()
-        self.remove_duplicates()
+	def validate(self):
+		self.calculate_total()
+		self.remove_duplicates()
 
-    def calculate_total(self):
-        total = self.flight_price or 0
+	def calculate_total(self):
+		total = self.flight_price or 0
 
-        for addon in self.add_ons:
-            total += addon.amount or 0
+		for addon in self.add_ons:
+			total += addon.amount or 0
 
-        self.total_price = total
+		self.total_price = total
 
-    def remove_duplicates(self):
-        seen = []
-        unique_rows = []
+	def remove_duplicates(self):
+		seen = []
+		unique_rows = []
 
-        for row in self.add_ons:
-            if row.item not in seen:
-                seen.append(row.item)
-                unique_rows.append(row)
+		for row in self.add_ons:
+			if row.item not in seen:
+				seen.append(row.item)
+				unique_rows.append(row)
 
-        self.set("add_ons", unique_rows)
+		self.set("add_ons", unique_rows)
 
-    def before_submit(self):
-        if self.status != "Boarded":
-           frappe.throw("Ticket can be submitted only when status is Boarded")
+	def before_submit(self):
+		if self.status != "Boarded":
+			frappe.throw("Ticket can be submitted only when status is Boarded")
 
-    def before_insert(self):
-        number = random.randint(1, 99)
-        letter = random.choice(["A", "B", "C", "D", "E"])
-        self.seat = f"{number}{letter}"
+	def before_insert(self):
+		number = random.randint(1, 99)
+		letter = random.choice(["A", "B", "C", "D", "E"])
+		self.seat = f"{number}{letter}"
+
+	def on_submit(self):
+		self.status = "Completed"
 
 
-    def on_submit(self):
-        self.status = "Completed"
+
+
+
+
+
