0e53abb2ba89c8f474f283d56cb4ab7f858f3c14
anuj-work-pro <anuj.tomar@promantia.com>
2026-05-04 23:50:44 +0530

    update www

--- airplane_mode/www/flight.py
+++
@@ -1,7 +0,0 @@
-import frappe
-
-
-def get_context(context):
-	flight_name = frappe.form_dict.get("name") or frappe.request.path.split("/")[-1]
-
-	context.flight = frappe.get_doc("Flight", flight_name)
